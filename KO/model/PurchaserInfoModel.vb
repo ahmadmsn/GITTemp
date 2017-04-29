6ebc87f2fa43ce39115a2c913b36e3060bf39c31
muahm1 <muahm1@ikea.com>
2017-04-29 12:39:29 +0200

    commit

---
+++ KO/model/PurchaserInfoModel.vb
@@ -0,0 +1,12 @@
+﻿Namespace ClientModels
+    Public Class PurchaserInfoModel
+        Inherits ClientModels.ClientModelBase
+        Public Property PurchaserId() As String
+        Public Property Name() As String
+        Public Property CASYUserId() As String
+        Public Property MISGroup() As String
+        Public Property RecieveMail() As Boolean
+        Public Property FoundRows() As String
+    End Class
+End Namespace
+
